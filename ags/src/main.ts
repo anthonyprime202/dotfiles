f735a45a7cc1ababa0c7dc9057b7d1ede5fc74b4
anthonyprime202 <anthonyprime202@gmail.com>
2024-04-10 03:15:26 +0530

    Added scripts

--- ags/src/main.ts
+++ ags/src/main.ts
@@ -4,7 +4,7 @@ const scss = App.configDir + '/src/styles/main.scss';
 const css = '/tmp/ags/styles.css'
 
 Utils.monitorFile(App.configDir + '/src/styles', async () => {
-    await Utils.execAsync(`scss ${scss} ${css}`)
+    await Utils.execAsync(`sassc ${scss} ${css}`)
 
     App.resetCss()
     App.applyCss(css)
