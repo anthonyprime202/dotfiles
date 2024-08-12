aad652ebf3579286be1af9fe99b2db043ea1bb76
anthonyprime202 <anthonyprime202@gmail.com>
2024-08-12 17:39:00 +0530

    Update few nvim tree settings

--- nvim/lua/setup/plugins/nvimtree.lua
+++ nvim/lua/setup/plugins/nvimtree.lua
@@ -4,17 +4,29 @@ return {
 		vim.g.loaded_netrw = 1
 		vim.g.loaded_netrwPlugin = 1
 		require("nvim-tree").setup({
-			sort = {
-				sorter = "case_sensitive",
+			update_cwd = true,
+			actions = {
+				open_file = {
+					resize_window = true,
+				},
 			},
 			view = {
-				width = 30,
+				side = "left",
 			},
-			renderer = {
-				group_empty = true,
+			update_focused_file = {
+				enable = true,
+				update_cwd = true,
 			},
 			filters = {
 				dotfiles = false,
+			},
+			git = {
+				enable = true,
+				ignore = false,
+				timeout = 500,
+			},
+			diagnostics = {
+				enable = true,
 			},
 		})
 	end,
