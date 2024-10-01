d47c688b405de12b16c3305ac55b36c508ebb4c8
anthonyprime202 <anthonyprime202@gmail.com>
2024-10-01 12:56:52 +0530

    Add clear function to the dnd feature

--- ags/src/widgets/bar/buttons.ts
+++ ags/src/widgets/bar/buttons.ts
@@ -58,7 +58,10 @@ export const ToolsTray = () =>
                         ? Icons.notification.off
                         : Icons.notification.on
                 }),
-                onClicked: () => (Notifications.dnd = !Notifications.dnd),
+                onClicked: async () => {
+                    await Notifications.clear()
+                    Notifications.dnd = !Notifications.dnd
+                },
             }),
         ],
     })
